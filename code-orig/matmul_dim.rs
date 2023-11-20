1bd8eec7d111447cbcf151195e4e42d85eae39de
Shlok Patel <101893296+Shlokkzz@users.noreply.github.com>
2023-11-20 13:09:54 +0530

    Update matmul_dim.rs

--- code-orig/matmul_dim.rs
+++ code-orig/matmul_dim.rs
@@ -1,28 +1,3 @@
-// extern crate ndarray;
-// use ndarray::prelude::*;
-// use rand::Rng;
-// use rand::distributions::Uniform; // Import Uniform from rand::distributions
-// use std::time::Instant;
-
-// fn main() {
-//     // Initialize the random number generator
-//     let mut rng = rand::thread_rng();
-
-//     // Generate two 800x800 matrices with random values
-//     let matrix_a: Array2<f64> = Array2::from_shape_fn((800, 800), |_| rng.sample(Uniform::new(0.0, 1.0)));
-//     let matrix_b: Array2<f64> = Array2::from_shape_fn((800, 800), |_| rng.sample(Uniform::new(0.0, 1.0)));
-
-//     // Measure the time it takes to perform the matrix multiplication
-//     let start = Instant::now();
-//     let result_matrix: Array2<f64> = matrix_a.dot(&matrix_b);
-//     let duration = start.elapsed();
-
-//     // Print the time taken
-//     println!("Time taken: {:?}", duration);
-
-//     // Ensure the result is computed to avoid optimization removing the computation
-//     println!("Result (first element): {}", result_matrix[(0, 0)]);
-// }
 extern crate ndarray;
 use ndarray::prelude::*;
 use rand::Rng;
